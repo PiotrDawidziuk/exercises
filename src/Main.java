741b0a26e7e013851daa02ef6b2f0f2073effc4f
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-07-21 23:07:12 +0200

    Streams added

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,14 +12,27 @@ public class Main {
             System.out.println(text);
         }
 
-        String s = "text";
+//        String s = "text";
+//
+//        System.out.println(s);
+//
+//        s = "text2";
+//
+//        System.out.println(s);
 
-        System.out.println(s);
 
-        s = "text2";
+        List<String> myList =
+                Arrays.asList("a1", "a2", "b1", "c2", "c1");
 
-        System.out.println(s);
-        
+//        myList
+//                .stream()
+//                .filter(s -> s.startsWith("c"))
+//                .map(String::toUpperCase)
+//                .sorted()
+//                .forEach(System.out::println);
+//
+        myList.stream().forEach(System.out::println);
+
 
     }
 }
