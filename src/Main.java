347812c48df0923b9079d942ff6a2ca84c1e0e34
PiotrDawidziuk <piotr.k.dawidziuk@gmail.com>
2018-07-21 22:20:18 +0200

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        String [] tabela = {"text1", "text2", "text3"};
+
+        for (String text: tabela) {
+            System.out.println(text);
+        }
+    }
+}
